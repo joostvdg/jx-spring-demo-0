82c7e2b9815eb74c9bf96c442a57630b051ac2d8
Joost van der Griendt <joostvdg@gmail.com>
2019-02-15 18:09:41 +0100

    Initial import

---
+++ src/main/kotlin/net/kearos/jx/jxspringdemo0/DemoApplication.kt
@@ -0,0 +1,12 @@
+package net.kearos.jx.jxspringdemo0
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DemoApplication>(*args)
+}
+
